be27b5afaea2c078684440e53482e71fb9614ffd
yamagai <kohei.yamamoto919@gmail.com>
2020-04-29 19:44:03 +0900

    CRUD機能コンプリート

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\BookRequest;
 use App\Http\Controllers\Controller;
 use App\Book;
 
@@ -24,5 +24,42 @@ class BookController extends Controller
 
       // 取得した値をビュー「book/edit」に渡す
       return view('book/edit', compact('book'));
+  }
+
+  public function update(BookRequest $request, $id)
+  {
+      $book = Book::findOrFail($id);
+      $book->name = $request->name;
+      $book->price = $request->price;
+      $book->author = $request->author;
+      $book->save();
+
+      return redirect("/book");
+  }
+
+  public function create()
+  {
+      // 空の$bookを渡す
+      $book = new Book();
+      return view('book/create', compact('book'));
+  }
+
+  public function store(BookRequest $request)
+  {
+      $book = new Book();
+      $book->name = $request->name;
+      $book->price = $request->price;
+      $book->author = $request->author;
+      $book->save();
+
+      return redirect("/book");
+  }
+
+  public function destroy($id)
+  {
+      $book = Book::findOrFail($id);
+      $book->delete();
+
+      return redirect("/book");
   }
 }
